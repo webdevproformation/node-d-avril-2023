8bc1c41df1631785abfae1432523d06cba16f041
Malik H <malik.h@webdevpro.net>
2023-04-17 15:00:10 +0200

    correction

---
+++ jour1/07-exo.js
@@ -0,0 +1,24 @@
+const { readFile } = require("node:fs")
+const { createServer } = require("node:http")
+const serveur = createServer();
+serveur.on("request", function(request, reponse){
+    if(request.url === "/"){
+        readFile("07-exemple.html" , "utf-8", function(err, fichier){
+            if(err) return console.log(err)
+            reponse.writeHead(200, {"Content-type" : "text/html; charset=uft-8"})
+            reponse.write(fichier)
+            reponse.end()
+        })
+    }
+    if(request.url === "/cocktails.json"){
+        readFile("07-data.json" , "utf-8", function(err, fichier){
+            if(err) return console.log(err)
+            reponse.writeHead(200, {"Content-type" : "application/json"})
+            reponse.write(fichier)
+            reponse.end()
+        })
+    }
+} )
+serveur.listen("4001", "localhost")
+
+
