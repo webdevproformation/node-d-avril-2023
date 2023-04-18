1dc61287a42bc080a8516f89aab3bbd8a78ebd6a
Malik H <malik.h@webdevpro.net>
2023-04-18 09:18:32 +0200

    start jour2

--- jour1-projet2/index.js
+++ jour1-projet2/index.js
@@ -3,4 +3,3 @@ import { writeFile } from "node:fs" // utilisation du système de module ESM
 writeFile("01-test.txt", "fin de journée" , "utf-8", function(){
     console.log("fin de journée");
 }); 
-
