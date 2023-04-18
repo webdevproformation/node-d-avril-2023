868ec0165f83f96e5871560ce62b65ac076d4dcb
Malik H <malik.h@webdevpro.net>
2023-04-18 16:56:19 +0200

    correction

--- jour2-tp/app.js
+++ jour2-tp/app.js
@@ -61,6 +61,62 @@ serveur.delete("/article/:id" , (request , reponse) => {
 
 // CRUD pour les commentaires 
 
+serveur.get("/commentaire/:idArticle" , (request , reponse) => {
+    const idArticle = request.params.idArticle ;
+    const article = rechercheArticle(articles , idArticle)
+    return reponse.json(article.commentaires)
+})
+serveur.post("/commentaire/:idArticle" , (request , reponse) => {
+    const idArticle = request.params.idArticle ;
+    const {body} = request
+    const article = rechercheArticle(articles , idArticle)
+    if(!article) return reponse.status(404).json({msg : "erreur 404"})
+    const index = articles.indexOf(article)
+    body.id = Date.now()
+    articles[index].commentaires.push(body)
+    return reponse.json({msg : "insert commentaire ok"})
+});
+
+serveur.put("/commentaire/:idArticle/:idCommentaire" , (request , reponse) => {
+    const idArticle = request.params.idArticle ;
+    const idCommentaire = request.params.idCommentaire ;
+    const {body} = request
+    const article = rechercheArticle(articles , idArticle)
+    if(!article) return reponse.status(404).json({msg : "erreur 404"})
+    const index = articles.indexOf(article)
+
+    const commentaireAModifier = articles[index].commentaires.find(function(commentaire){
+        return commentaire.id == idCommentaire
+    })
+    if(!commentaireAModifier) return reponse.status(404).json({msg : "erreur 404 commentaire introuvable"})
+
+    const indexCommentaire =  articles[index].commentaires.indexOf(commentaireAModifier)
+
+    articles[index].commentaires[indexCommentaire].auteur = body.auteur
+    articles[index].commentaires[indexCommentaire].message = body.message
+    
+    return reponse.json({msg : "update commentaire ok"})
+})
+
+serveur.delete("/commentaire/:idArticle/:idCommentaire" , (request , reponse) => {
+    const idArticle = request.params.idArticle ;
+    const idCommentaire = request.params.idCommentaire ;
+
+    const article = rechercheArticle(articles , idArticle)
+    if(!article) return reponse.status(404).json({msg : "erreur 404"})
+    const index = articles.indexOf(article)
+
+    const commentaireAModifier = articles[index].commentaires.find(function(commentaire){
+        return commentaire.id == idCommentaire
+    })
+    if(!commentaireAModifier) return reponse.status(404).json({msg : "erreur 404 commentaire introuvable"})
+    const indexCommentaire =  articles[index].commentaires.indexOf(commentaireAModifier)
+
+    articles[index].commentaires.splice(indexCommentaire , 1)
+
+    return reponse.json({msg : "delete commentaire ok"})
+})
+
 
 
 serveur.listen(PORT , () => console.log(`le serveur express est start sur le port ${PORT}`))
