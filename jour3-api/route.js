355e9ab5682c579f57ec0cd02e6dc7650141908d
Malik H <malik.h@webdevpro.net>
2023-04-21 11:30:10 +0200

    fin des middleware

--- jour3-api/route.js
+++ jour3-api/route.js
@@ -1,7 +1,7 @@
 const { Router } = require("express")
 const { Article } = require("./model") 
 
-const {traitement1 , traitement2 , idValid } = require("./middleware")
+const {traitement1 , traitement2 , idValid , isValidArticle } = require("./middleware")
 
 const route = Router();
 // fetch("http://localhost:4003", {method: "GET"})=> récupérer du serveur
@@ -12,14 +12,8 @@ route.get("/", function(request, reponse){
     reponse.json({msg : "fonction"})
 })
 // route.post("/" , function(request, reponse){})
-route.post("/" , async function(request, reponse){
+route.post("/" , isValidArticle ,  async function(request, reponse){
     const { body } = request; 
-
-    /* console.log(error);
-    return reponse.json("stop"); */
-    const {error} = schemaArticleJoi.validate(body , { abortEarly : false})
-    if(error) return  reponse.status(400).json(error.details) // 400 Bad Request
-
     const newArticle = new Article(body) // exos.push(body)
     await newArticle.save() // MongoDB => traitements qui sont asynchrones  => await 
     reponse.json(newArticle); 
@@ -37,7 +31,7 @@ route.post("/" , async function(request, reponse){
 // récupérer tous les articles
 // GET http://localhost:4003/all
 // 1 middleware 
-route.get("/all", traitement1 , async (request, reponse) => {
+route.get("/all" , async (request, reponse) => {
    const tousLesArticles = await Article.find()
    reponse.json(tousLesArticles); 
 })
@@ -69,19 +63,13 @@ route.get("/:id", idValid , async (request , reponse) => {
 // put => update sur TOUS les champs de l'article titre / auteur / contenu
 // patch => update sur certains champs de l'article
 
-route.put("/:id" , idValid , async (request , reponse) => {
+route.put("/:id" , [idValid, isValidArticle ] , async (request , reponse) => {
     const id = request.params.id ;
-
     const { body } = request ;
-
-    const {error} = schemaArticleJoi.validate(body , { abortEarly : false})
-    if(error) return  reponse.status(400).json(error.details) // 400 Bad Request
-
     // effectue l'update 
     const articleUpdated = await Article.findByIdAndUpdate(id , { $set : body } , { new : true})
     // { new : true} est facultatif => il permet de récupérer l'article avec les updates 
     // sans cette valeur => findByIdAndUpdate retourne l'ancienne valeur de l'article mis à jour
-
     if(!articleUpdated) return reponse.status(404).json({ msg : `l'article ${id} n'existe pas` }) ; 
 
     reponse.json(articleUpdated)
